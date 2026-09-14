43b6203170e8ba2156ca065d73480a8f4a8bb7ff
Anii71128 <aniatunnikmah123@gmail.com>
2026-09-14 07:51:19 +0700

    feat: implement UI searchbar

---
+++ resources/js/components/ui/SearchBar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="relative w-full max-w-180">
+        <img
+            src="/icons/search.svg"
+            alt=""
+            class="pointer-events-none absolute left-6 top-1/2 h-7 w-7 -translate-y-1/2 object-contain"
+        />
+
+        <input
+            type="text"
+            placeholder="Search"
+            class="h-16 w-full rounded-full border-[3px] border-black bg-white pl-[68px] pr-6 text-[18px] text-black outline-none transition-all duration-200 placeholder:text-gray-500 focus:border-black"
+        />
+    </div>
+</template>
--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -1,11 +1,16 @@
 <script setup>
+import SearchBar from "../components/ui/SearchBar.vue";
+
 defineProps({
     pesan: String,
 });
 </script>
 
 <template>
-    <div class="p-8">
-        <h1 class="text-3xl font-bold text-blue-600">{{ pesan }}</h1>
-    </div>
+    <main class="p-8">
+
+        <div class="mt-8 max-w-xl">
+            <SearchBar />
+        </div>
+    </main>
 </template>
